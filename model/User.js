e54435c10ecd4b736291894d2fd773e16a5bb86d
Jonathan Tumalle <jonathantumalle@Jonathans-MBP.fios-router.home>
2020-05-16 15:34:17 -0400

    Registration and logging in now works with database with joi and bcrypt validation

--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
 
+// MongoDB User Document
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -15,13 +17,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         max: 256,
-        min: 6
+        min: 1
     },
     password: {
         type: String,
         required: true,
         max: 1024,
-        min: 6
+        min: 8
     },
     date: {
         type: Date,
@@ -29,5 +31,72 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('User', userSchema);
+// Joi schemas for validation
+const registerSchema = Joi.object({
+    firstName: Joi.string()
+        .min(1)
+        .max(256),
+    lastName: Joi.string()
+        .min(1)
+        .max(256),
+    email: Joi.string()
+        .email()
+        .required(),
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{8,30}$'))
+        .messages({
+            'string.pattern.base': "Invalid Password"
+        }),
+    passwordConfirm: Joi.any()
+        .equal(Joi.ref('password'))
+        .messages({
+            'any.only': "Passwords must match"
+        })
+        
+}).with('password', 'passwordConfirm')
+    .with('firstName','lastName');
+
+const loginSchema = Joi.object({
+    email: Joi.string()
+        .email(),
+    password: Joi.string()
+}).with('email', 'password');
+
+// -----Middleware-----
+// Registration
+registerValidation = async (req, res, next) => {
+
+    // Validate request structure
+    const {error} = registerSchema.validate(req.body);
+    if(error) {
+        error.details.forEach((detail) => console.log(detail.message));
+        return res.status(400).redirect('/users/register');
+    }
+
+    // Ensure email is unique
+    const User = mongoose.model('User', userSchema);
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists) {
+        return res.status(400).send("Email already exists")
+    }
+
+    next();
+};
+
+loginValidation = async (req, res, next) => {
+    const {error} = await loginSchema.validateAsync(req.body);
+    if(error) {
+        error.details.forEach((detail) => console.log(detail.message));
+        return res.status(400).redirect('/users/login');
+    }
+
+    next();
+};
+
+
+module.exports = {
+    User: mongoose.model('User', userSchema),
+    checkValidNewUser: registerValidation,
+    checkValidUser: loginValidation
+};
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,33 @@
-const User = require('../model/User');
+const {User, checkValidNewUser, checkValidUser} = require('../model/User');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
 // Register
 router
     .get('/register', (req, res) => {
         res.render('register');
     })
-    .post('/register', (req,res) => {
-        const {firstName, lastName, email, password, passwordConfirm} = req.body;
-        const newUser = new User({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password
+    .post('/register', checkValidNewUser, (req,res) => {
+        const {firstName, lastName, email, password} = req.body;
+
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+            if(err) {
+                // Error with creating bcrypt hash
+                console.log(err);
+                return res.status(400).redirect('/register');
+            }
+
+            // Create user document with hash and save in db
+            const newUser = new User({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: hash
+            });
+            newUser.save();
+            return res.status(200).send("Thanks for registering"+ firstName);
         });
-        console.log(newUser);
-        res.end();
     });
 
 // Login
@@ -23,9 +35,23 @@ router
     .get('/login', (req,res) => { 
         res.render('login'); 
     })
-    .post('/login', (req,res) => {
-        console.log(req.body);
-        res.end();
+    .post('/login', checkValidUser, async (req,res) => {
+        const {email, password} = req.body;
+        const loginUser = await User.findOne({email: email});
+        if(!loginUser) {
+            return res.status(400).send('Email does not exist');
+        }
+
+        const validPassword = await bcrypt.compare(password, loginUser.password);
+        if(validPassword){
+            return res.status(200).send("Welcome back "+loginUser.firstName);
+        } else{
+            return res.status(400).send("Password incorrect");
+        }
+
     });
+
+
+
 
 module.exports = router;
